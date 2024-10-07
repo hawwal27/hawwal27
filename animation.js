ac770e32a686399cc812465630e17c1519b18fbf
hawwal27 <ijaolahawwal@gmail.com>
2024-10-07 06:51:26 -0800

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,22 @@
+const container= document.querySelector('.container-circle')
+container.addEventListener(
+    'animationstart',
+    (e)=>{
+        //do something
+    },
+    false
+)
+container.addEventListener(
+    'animationend',
+    (e)=>{
+        // do something
+    },
+    false
+)
+container.addEventListener(
+    'animationiteration',
+    (e) =>{
+        //do something
+    },
+    false
+)
